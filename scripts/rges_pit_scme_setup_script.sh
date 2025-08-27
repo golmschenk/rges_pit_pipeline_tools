13e9d8e6819b1bfa3fe8fefa9103a66c6fbba363
golmschenk <greg@olmschenk.com>
2025-08-27 15:25:05 -0400

    Remove extra logging

--- scripts/rges_pit_scme_setup_script.sh
+++ scripts/rges_pit_scme_setup_script.sh
@@ -2,14 +2,13 @@
 
 echo "Starting SMCE setup script."
 if ! grep -qF '# >>> RGES-PIT setup start >>>' "${HOME}/.bashrc"; then
-  set -x
   mkdir -p /shared/"${USER}"/.conda
   ln -s /shared/"${USER}"/.conda "${HOME}"/.conda
   source /shared/spack/share/spack/setup-env.sh
   spack load miniforge3
   conda init
   conda config --set auto_activate_base false
-  conda create -n default_env python=3.13
+  conda create -n default_env python=3.13 -y -q
   cat >> "${HOME}/.bashrc" << 'EOF'
 # >>> RGES-PIT setup start >>>
 case $- in
@@ -23,6 +22,6 @@ conda activate default_env
 # <<< RGES-PIT setup end <<<
 EOF
   source "${HOME}/.bashrc"
-  set +x
 fi
 echo "SMCE setup script complete."
+echo "Run \`source ~/.bashrc\` now or restart the terminal session."
